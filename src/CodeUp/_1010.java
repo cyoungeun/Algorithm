8dc34f2c1b61c5579b5945bb5a3c1f93be0ebb9e
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-08-30 23:04:32 +0900

    1010 - 정수 1개 입력받아 그대로 출력

---
+++ src/CodeUp/_1010.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1010 {
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        System.out.println(n);
+    }
+}
