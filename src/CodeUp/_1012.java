674f563063b3064b06f7013f9277659d72985fef
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-02 02:54:26 +0900

    1012 - 실수(float) 1개 입력받아 그대로 출력

---
+++ src/CodeUp/_1012.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1012 {
+    public static void main(String[] args) {
+        float f;
+        Scanner scanner = new Scanner(System.in);
+        f = scanner.nextFloat();
+        System.out.println(f);
+    }
+}
