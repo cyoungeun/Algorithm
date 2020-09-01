dfe249a0a78a2e61ab0c415915361820d97cc36c
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-02 02:38:37 +0900

    1011 - 문자(char) 1개 입력받아 그대로 출력

---
+++ src/CodeUp/_1011.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1011 {
+    public static void main(String[] args) {
+        char x;
+        Scanner scanner = new Scanner(System.in);
+        x = scanner.next().charAt(0);
+        System.out.println(x);
+    }
+}
