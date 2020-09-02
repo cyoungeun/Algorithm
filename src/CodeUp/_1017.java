0f1385b1674ff1639ebbc4e7fb3fed58771788ed
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-02 23:04:30 +0900

    1017 - 정수 1개 입력받아 공백사이에 두고 3번 출력

---
+++ src/CodeUp/_1017.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1017 {
+    public static void main(String[] args) {
+        int a;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        System.out.printf("%d %d %d", a, a, a);
+    }
+}
