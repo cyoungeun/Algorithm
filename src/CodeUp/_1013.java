09677de13503373cc532c02181fc08c1f1687b82
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-02 21:45:49 +0900

    1013 - 정수 2개를 입력받아 그대로 출력(공백으로 구분되어 입력)

---
+++ src/CodeUp/_1013.java
@@ -0,0 +1,13 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1013 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.printf("%d %d", a, b);
+    }
+}
