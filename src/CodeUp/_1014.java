d96d2035e082c9279af0a9d79cf76b6a025f090b
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-02 22:11:02 +0900

    1014 - 2개의 문자(ASCII CODE)를 입력받아 순서 바꿔 출력

---
+++ src/CodeUp/_1014.java
@@ -0,0 +1,13 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1014 {
+    public static void main(String[] args) {
+        char x, y;
+        Scanner scanner = new Scanner(System.in);
+        x = scanner.next().charAt(0);
+        y = scanner.next().charAt(0);
+        System.out.printf("%c %c", y, x);
+    }
+}
