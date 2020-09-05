0ccd3f253444672482bba44427c5b60d405c5d32
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-06 02:15:02 +0900

    1021 - 단어 1개 입력받아 출력 (단어의 길이는 50자 이하)

---
+++ src/CodeUp/_1021.java
@@ -0,0 +1,13 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1021 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String c = scanner.next();
+        if(c.length() <= 50){
+            System.out.printf("%s", c);
+        }
+    }
+}
