df185b8de902fe43f3f240db50802de2310b6a4c
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-06 23:57:51 +0900

    1022 - 공백이 포함된 문장 1개 입력받아 그대로 출력 (단어 최대 길이는 2000자)

---
+++ src/CodeUp/_1022.java
@@ -0,0 +1,13 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1022 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String c = scanner.nextLine();
+        if(c.length() <= 2000){
+            System.out.printf("%s", c);
+        }
+    }
+}
