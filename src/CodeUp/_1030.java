afda90329968459a0696c2d5487e5d740e9688ba
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-09 23:54:38 +0900

    1030 - 정수 1개 입력받아 출력(입력되는 정수의 범위는 -9,223,372,036,854,775,808 ~ +9,223,372,036,854,775,807)

---
+++ src/CodeUp/_1030.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1030 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long x = scanner.nextLong();
+        System.out.printf("%d", x);
+    }
+}
