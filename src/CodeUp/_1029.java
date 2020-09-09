f5daa71c08a73e8ab34cd4f2fa197902978ad5d5
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-09 23:53:34 +0900

    1029 - 소수점 아래 숫자가 11개 이하인 실수 1개 입력받아 출력(입력되는 실수의 범위는 +- 1.7*10-308 ~ +- 1.7*10308)

---
+++ src/CodeUp/_1029.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1029 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        System.out.printf("%.11f", x);
+    }
+}
