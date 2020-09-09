5a9cdd731b0fd1aa87a338893a92b9ec5157edca
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-09 23:59:15 +0900

    1034 - 8진수 1개를 입력받아 10진수로 출력

---
+++ src/CodeUp/_1034.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1034 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String x = scanner.next();
+        int a = Integer.valueOf(x, 8);
+        System.out.printf("%d", a);
+    }
+}
