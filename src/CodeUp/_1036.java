d463baeb88286781f82fd5e8741ec317db35ae7d
최영은 <youngxxeun@gmail.com>
2020-09-09 16:28:48 +0000

    1036 - 영문자 1개를 입력받아 아스키 코드표의 10진수 값으로 출력

---
+++ src/CodeUp/_1036.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1036 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char a = scanner.next().charAt(0);
+        int b = (int)a;
+        System.out.println(b);
+    }
+}
