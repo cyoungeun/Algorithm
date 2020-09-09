277d257f50dd2d99167754c2386ab930fcad4e2e
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-09 23:55:36 +0900

    1031 - 10진수 1개를 입력받아 8진수로 출력

---
+++ src/CodeUp/_1031.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1031 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.printf("%o", x);
+    }
+}
