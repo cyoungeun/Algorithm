5cd6376dbd4e38e7d3ab690db103df99b7e8157b
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-09 23:58:05 +0900

    1033 - 10진수 1개를 입력받아 16진수(대문자)로 출력

---
+++ src/CodeUp/_1033.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1033 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.printf("%X", x);
+    }
+}
