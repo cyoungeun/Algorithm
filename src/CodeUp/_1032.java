306bdd6b9c3c79bfe0b01b9593df0227a5f03afb
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-09 23:57:08 +0900

    1032 - 10진수 1개를 입력받아 16진수로 출력

---
+++ src/CodeUp/_1032.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1032 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.printf("%x", x);
+    }
+}
