abadc02fa7d281d0fcb174e139410c8750d5df8b
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-10 17:06:54 +0900

    1038 - 정수 2개를 입력받아 두 정수의 합을 출력(입력 정수의 범위는 -1073741824~1073741824)

---
+++ src/CodeUp/_1038.java
@@ -0,0 +1,14 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1038 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.printf("%.0f", a+b);
+
+    }
+}
