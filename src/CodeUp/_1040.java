f01d50002bc59bedf701908891e6145ea4f865bd
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-12 00:24:10 +0900

    1040 - 정수 1개를 입력받아 부호 바꿔 출력

---
+++ src/CodeUp/_1040.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1040 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        System.out.println(-a);
+    }
+}
