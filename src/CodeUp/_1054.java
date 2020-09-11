431e0acb3937816698ce4919432ec987a455019c
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-12 03:18:50 +0900

    1054 - 2개의 참(1) 또는 거짓(0)이 입력될 때, 모두 참(1)일 때에만 참(1)을 출력

--- src/CodeUp/_1054.java
+++ src/CodeUp/_1054.java
@@ -6,7 +6,7 @@ public class _1054 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        int b= scanner.nextInt();
+        int b = scanner.nextInt();
         if (a == 1 && b == 1){
             System.out.println(1);
         }else{
