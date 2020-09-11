90e9d371d565b8a244214e924822c5b547726148
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-12 00:37:52 +0900

    1041 - 영문자 1개를 입력받아 그 다음 문자 출력

---
+++ src/CodeUp/_1041.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1041 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char x = scanner.next().charAt(0);
+        System.out.printf("%c", x+1);
+    }
+}
