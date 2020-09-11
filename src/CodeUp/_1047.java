29ca8932127666def6499c6b1ded6e04c022cfd7
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-12 01:41:22 +0900

    1047 - 정수 1개를 입력받아 2배 곱한 정수 출력

---
+++ src/CodeUp/_1047.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1047 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.printf("%d", a<<1);
+    }
+}
