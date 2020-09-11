5110ee99265c34fcb258ba3efe902d9216010e03
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-12 00:51:29 +0900

    1042 - 정수 2개를 입력받아 나눈 몫 출력

---
+++ src/CodeUp/_1042.java
@@ -0,0 +1,15 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1042 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        int data = a/b;
+        System.out.println(data);
+    }
+}
