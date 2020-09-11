1c72ff14a31feb43d7f4e94e20e232a7083cfc01
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-12 02:10:28 +0900

    1048 - 정수 2개를 입력받아 2의 거듭제곱 배로 출력

---
+++ src/CodeUp/_1048.java
@@ -0,0 +1,13 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1048 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.printf("%d", a<<b);
+    }
+}
