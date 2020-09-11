29e14a171a1d61a312cf96ae7fe81c794506e80f
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-12 03:08:33 +0900

    1053 - 1(true, 참) 또는 0(false, 거짓) 이 입력되었을 때 반대로 출력

---
+++ src/CodeUp/_1053.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1053 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a == 1 ? 0 : 1);
+    }
+}
