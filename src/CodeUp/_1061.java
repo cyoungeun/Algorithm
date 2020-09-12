67d8108c482ee5f4dfc0195918e9bb9e8ad1b8af
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-12 23:54:24 +0900

    1061 - 비트 단위로  OR 하여 그 값을 10진수로 출력

---
+++ src/CodeUp/_1061.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1061 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a | b);
+    }
+}
