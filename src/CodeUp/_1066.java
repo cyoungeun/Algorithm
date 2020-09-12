28dd706fc96105a0a8f2672ed0fb5bf2aade6448
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-13 00:38:38 +0900

    1066 - 정수 3개를 입력받아 짝(even) / 홀(odd)로 구분하여 출력

---
+++ src/CodeUp/_1066.java
@@ -0,0 +1,30 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1066 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if(a % 2 == 0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+
+        if(b % 2 == 0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+
+        if(c % 2 == 0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
