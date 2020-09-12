2c9e00ed79895655b08af90fb85d7fea474a4a75
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-13 01:29:40 +0900

    1071 - 0이 아니면 입력된 정수를 줄을 바꿔 하나씩 출력, 0이 입력되면 출력 종료

---
+++ src/CodeUp/_1071.java
@@ -0,0 +1,18 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1071 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a;
+
+        while (true){
+            a = scanner.nextInt();
+            if(a == 0){
+                break;
+            }
+            System.out.println(a);
+        }
+    }
+}
