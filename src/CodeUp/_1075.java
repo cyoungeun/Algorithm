42828ef27c00381dddae669cb6eb2eee89ffd88d
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-14 00:41:03 +0900

    1075 - 정수(1 ~100) 1개 입력받아 카운트다운 출력 (0이 될 때까지)

---
+++ src/CodeUp/_1075.java
@@ -0,0 +1,15 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1075 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        while (a > 0 && a <= 100){
+            a--;
+            System.out.println(a);
+        }
+    }
+}
