66b429dcd7c247330f61b5644e46de1e5881afc9
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-09-14 00:35:39 +0900

    1074 - 정수(1 ~100) 1개 입력받아 카운트다운 출력

---
+++ src/CodeUp/_1074.java
@@ -0,0 +1,15 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1074 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        while (a > 0 && a <= 100){
+            System.out.println(a);
+            a--;
+        }
+    }
+}
