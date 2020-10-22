9f071b56ec847356da92e43a8590d5f073159164
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-10-22 20:51:03 +0900

    1094 - 첫 줄에 출석 번호를 부른 횟수인 정수 n, 두번째 줄에는 n개의 랜덤 번호(k, 1 ~ 23)가  순서대로 입력되면 부른 랜덤 번호를 거꾸로 출력

--- src/CodeUp/_1094.java
+++ src/CodeUp/_1094.java
@@ -1,2 +1,21 @@
-package CodeUp;public class _1094 {
+package CodeUp;
+
+import java.util.Scanner;
+
+public class _1094 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int k[] = new int[n];
+        for (int i = 0; i < n; i++){
+            k[i] = scanner.nextInt();
+        }
+
+        for (int j = k.length-1; j >= 0; j--){
+            System.out.printf("%d ", k[j]);
+        }
+
+
+    }
 }
