372a277f3f74a5c7c4a4cb72cc1a916d92cd3260
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-10-26 20:51:18 +0900

    10171 - 고양이 출력

---
+++ src/BOJ/L1/_10171.java
@@ -0,0 +1,10 @@
+package BOJ.L1;
+
+public class _10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
