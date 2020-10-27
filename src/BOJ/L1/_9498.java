588591f1a3a0b48ff30e4964a2a99375e66fd03f
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-10-27 11:39:54 +0900

    9498 - 시험 점수를 입력받아 해당 점수의 성적을 출력

---
+++ src/BOJ/L1/_9498.java
@@ -0,0 +1,24 @@
+package BOJ.L1;
+
+import java.util.Scanner;
+
+public class _9498 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt();
+
+        if (score >= 0 && score <= 100){
+            if (score >= 90){
+                System.out.println("A");
+            }else if (score >= 80 && score <= 89){
+                System.out.println("B");
+            }else if (score >= 70 && score <= 79){
+                System.out.println("C");
+            }else if (score >= 60 && score <= 69){
+                System.out.println("D");
+            }else{
+                System.out.println("F");
+            }
+        }
+    }
+}
