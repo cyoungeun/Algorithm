adf983c784168269b57b9c235abde4154abd77c4
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-10-29 22:19:10 +0900

    2438 - 첫번째 줄부터 n번째 줄까지 차례대로 별 출력 (별 찍기1)

---
+++ src/BOJ/L3/_2438.java
@@ -0,0 +1,17 @@
+package BOJ.L3;
+
+import java.util.Scanner;
+
+public class _2438 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
