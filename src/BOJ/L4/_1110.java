a22d50bc3dd5a35100b453475b738fe9ed561808
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-10-30 19:06:35 +0900

    1110 - 정수 n을 입력받고 n이 10보다 작다면 앞에 0을 붙여 두 자리 수로 만들고, 각 자리의 숫자를 더한 후 주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪽 자리 수를 이어 붙이면 새로운 수를 구할 수 있다. 처음 입력받은 수와 새로운 수가 같아지는 사이클 길이(i)를 출력

---
+++ src/BOJ/L4/_1110.java
@@ -0,0 +1,25 @@
+package BOJ.L4;
+
+import java.util.Scanner;
+
+public class _1110 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); // 입력받는 수
+        int num = n;
+        int i = 0; // 사이클 길이
+
+        while(true){
+            int n1 = n / 10; 
+            int n2 = n % 10;
+            int n3 = n1 + n2;
+            n = ((n2) * 10) + ((n3) % 10);
+            i++;
+
+            if(num == n){
+                break;
+            }
+        }
+        System.out.println(i);
+    }
+}
