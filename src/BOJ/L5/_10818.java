fda138eba289dcbb2ae806c3ef963fbffa1e6b84
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-11-02 22:14:21 +0900

    10818 - 정수 n과 n개의 정수를 입력 받은 후, 정수 n개의 최솟값과 최댓값을 출력

---
+++ src/BOJ/L5/_10818.java
@@ -0,0 +1,27 @@
+package BOJ.L5;
+
+import java.util.Scanner;
+
+public class _10818 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int min = arr[0];
+        int max = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] < min){ // 최솟값
+               min = arr[i];
+            }
+            if(arr[i] > max){ // 최댓값
+                max = arr[i];
+            }
+        }
+        System.out.print(min + " " + max);
+    }
+}
