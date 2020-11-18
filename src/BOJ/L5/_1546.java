1726ef7b92d696a77000807269ecc1e9207dd04f
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-11-18 17:13:21 +0900

    1546 - 시험과목 수 n과 n개 과목의 점수를 입력 받았을 때, n개 과목 중 최댓값 max를 골라 모든 점수를 해당 식(점수/max*100)으로 고친 후 새로운 평균 값을 출력

---
+++ src/BOJ/L5/_1546.java
@@ -0,0 +1,31 @@
+package BOJ.L5;
+
+import java.util.Scanner;
+
+public class _1546 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int scoreArr[] = new int[n];
+        float xScore[] = new float[n];
+        float max = scoreArr[0];
+        float sum = 0;
+        float avg = 0;
+
+        for(int i = 0; i < n; i++) {
+            int score = scanner.nextInt();
+            scoreArr[i] = score;
+
+            if(scoreArr[i] > max) {
+                max = scoreArr[i];
+            }
+        }
+
+        for(int i = 0; i < scoreArr.length; i++) {
+            xScore[i] = scoreArr[i] / max * 100;
+            sum += xScore[i];
+            avg = sum / n;
+        }
+        System.out.println(avg);
+    }
+}
