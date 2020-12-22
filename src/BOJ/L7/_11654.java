3e1d834db013ce747abbd706e1f91be3b4746eea
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2020-12-22 18:01:55 +0900

    11654 - 아스키 코드 알파벳 소/대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력

---
+++ src/BOJ/L7/_11654.java
@@ -0,0 +1,13 @@
+package BOJ.L7;
+
+import java.util.Scanner;
+
+public class _11654 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 문자열로 입력받아 charAt()으로 잘라내어 문자로 변환
+        int c = scanner.next().charAt(0);
+        System.out.println(c);
+    }
+}
