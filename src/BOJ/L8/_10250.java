6216f36bbc077f6b7045ee004baee57991dcdcba
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2021-01-06 17:06:51 +0900

    10250 - ACM 호텔 테스트케이스(t), 각각 호텔 층 수(h), 각 층의 방 수(w), n번째 손님 을 입력 받아 n번째 손님에게 배정되어야 하는 방 번호 출력

---
+++ src/BOJ/L8/_10250.java
@@ -0,0 +1,36 @@
+package BOJ.L8;
+
+import java.util.Scanner;
+
+public class _10250 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        for(int i = 0; i < t; i++) {
+            int h = scanner.nextInt(); // 각각 호텔의 층 수 h
+            int w = scanner.nextInt(); // 각 층에 w개의 방
+            int n = scanner.nextInt(); // n번째 손님
+
+            // 방 번호 형태 yxx or yyxx
+            int nNum = 1; // n번째 손님 찾아줄 변수
+            int xx = 1; // 호수
+
+            while (xx <= w) { // 호수가 각 층 w개의 방 이하일 경우
+                for(int y = 1; y <= h; y++) { // 1 ~ h층 까지 반복
+
+                    if(nNum == n){
+                        System.out.printf("%d%02d \n", y, xx); // 배정 될 방 번호
+                    }
+
+                    if(y == h) { // 입력받은 h층과 같아지면 호수+1
+                        xx++;
+                    }
+
+                    nNum++; // 배정 될 때 마다 +1
+                }
+            }
+        }
+
+    }
+}
