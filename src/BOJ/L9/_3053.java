02c40504653269723010eea60c86c510e4cb2fc4
cyoungeun <45746241+cyoungeun@users.noreply.github.com>
2021-01-16 23:41:34 +0900

    3053 -  택시 기하학 반지름(r)이 주어졌을 때, 첫째 줄에는 유클리드 기하학에서의 원의 넓이를, 둘째 줄에는 택시 기하학에서의 원의 넓이를 출력

---
+++ src/BOJ/L9/_3053.java
@@ -0,0 +1,19 @@
+package BOJ.L9;
+
+import java.util.Scanner;
+
+public class _3053 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 반지름 r
+        int r = scanner.nextInt();
+
+        double circleU = r * r * Math.PI; // 유클리드 기하학의 원의 넓이
+        double circleT = r * r * 2; // 택시 기하학의 원의 넓이
+
+        System.out.printf("%.6f \n", circleU);
+        System.out.printf("%.6f", circleT);
+
+    }
+}
